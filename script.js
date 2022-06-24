ca8314b9187be08d6c0a8de55202de5fd3f9d73e
Slava <example@gmail.com>
2022-06-24 20:48:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+"use strict";    //режим современного кода
+//
+// let number = 4.6;
+//
+// console.log('string' * 9); // NaN
+// console.log('4/0') // бесконечность
+//
+// const persone = "Alex";
+//
+// const bool = true;
+//
+// console.log(something); // null - когда объекта не существует(ошибка)
+//
+// let und;
+// console.log(und); //undefined - когда объекту не задано значение
+//
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+//
+// console.log(obj.name);
+//
+// let array = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {} ];
+// console.log(array[1]);
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+//     console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+//
+// console.log(`https://someurl.com/${category}/5`);
+//
+// const user = "Ivan";
+//
+// alert(`Привет, ${user}`);
+
+console.log('array' + " - object");
+console.log(4 + +"5");
+
+ let incr = 10,
+     decr = 10;
+
+//incr++; // оператор инкримента(увелечение на 1)
+//decr--; // операдор декримента(уменьшение на 1)
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 == 8);      // === - строгое сравнение
+
+const isChecked = true,
+    isClose = true;
+
+console.log(isChecked || isClose);    // true будет если все переменные true, если хотябы 1 нет, то false
+                                       // || - или
+
+// ! - оператор который переворачивает значение на противоположное
+
+
+
